dbd53fcd09e9cb319c2e2aff30414e8762d99902
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-07-14 19:40:16 -0400

    For the scope of this project, having a Input component is too overkill. The component was deleted and replaced with two input fields inside of the MemeInputs component. The state for these two inputs and for the memes that are being obtained from the API was created

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import GenerateMeme from "./components/GenerateMeme";
 import { useState, useEffect } from "react";
 
 const App = () => {
-    // Applications state
+    // State of the application
+    const [memes, setMemes] = useState([]);
     const [meme, setMeme] = useState("https://i.imgflip.com/9ehk.jpg");
+    const [topText, setTopText] = useState("");
+    const [bottomText, setBottomText] = useState("");
 
     // Fetch API function
     const fetchAPI = async () => {
         const res = await fetch("https://api.imgflip.com/get_memes");
         const data = await res.json();
         const memesArray = data.data.memes;
-        console.log(memesArray)
+        const memesWithTwoBoxes = memesArray.filter(meme => meme.box_count === 2);
+        setMemes(memesWithTwoBoxes);
     };
 
     // Fetch memes API on page load
@@ -20,10 +24,14 @@ const App = () => {
         fetchAPI();
     }, []);
 
+    const handleTextChange = (e) => {
+        e.target.classList.contains("input--top-text") ? setTopText(e.target.value) : setBottomText(e.target.value);
+    };
+
     return (
         <div className="App">
             <Header />
-            <MemeInputs />
+            <MemeInputs topText={topText} bottomText={bottomText} handleChange={handleTextChange} />
             <GenerateMeme memeTemplate={meme} />
         </div>
     );
--- src/components/MemeInputs.jsx
+++ src/components/MemeInputs.jsx
@@ -1,10 +1,8 @@
-import Input from "./Input";
-
-const MemeInputs = () => {
+const MemeInputs = ({ topText, bottomText, handleChange }) => {
     return (
         <div className="flex flex-col gap-2 px-4 pt-10 pb-5">
-            <Input />
-            <Input />
+            <input type="text" onChange={(e) => handleChange(e)} value={topText} placeholder="Enter text..." className="input--top-text block p-1 w-full border-2 border-gray-500 rounded-md" />
+            <input type="text" onChange={(e) => handleChange(e)} value={bottomText} placeholder="Enter text..." className="input--bottom-text block p-1 w-full border-2 border-gray-500 rounded-md" />
         </div>
     );
 };
