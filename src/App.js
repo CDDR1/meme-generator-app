fd7102421c8c240f7b6f5b67ffeacf6e5c1a058b
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-07-14 18:59:37 -0400

    Added meme state and API fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Header from "./components/Header";
 import MemeInputs from "./components/MemeInputs";
 import GenerateMeme from "./components/GenerateMeme";
+import { useState, useEffect } from "react";
 
 const App = () => {
+    // Applications state
+    const [meme, setMeme] = useState("https://i.imgflip.com/9ehk.jpg");
+
+    // Fetch API function
+    const fetchAPI = async () => {
+        const res = await fetch("https://api.imgflip.com/get_memes");
+        const data = await res.json();
+        const memesArray = data.data.memes;
+        console.log(memesArray)
+    };
+
+    // Fetch memes API on page load
+    useEffect(() => {
+        fetchAPI();
+    }, []);
+
     return (
         <div className="App">
             <Header />
             <MemeInputs />
-            <GenerateMeme />
+            <GenerateMeme memeTemplate={meme} />
         </div>
     );
 };
--- src/components/GenerateMeme.jsx
+++ src/components/GenerateMeme.jsx
@@ -1,8 +1,8 @@
-const GenerateMeme = () => {
+const GenerateMeme = ({ memeTemplate }) => {
     return (
         <div className="flex flex-col items-center gap-3 px-4">
             <div>
-                <img src="https://via.placeholder.com/280" alt="Meme template" className="block w-full" />
+                <img src={memeTemplate} alt="Meme template" className="block w-full" />
             </div>
             <button className="font-semibold bg-gray-300 p-2 block w-full rounded-lg">Generate meme template</button>
         </div>
