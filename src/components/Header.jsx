e81adac5881638a3b4ade6bd8f7ef593fef4c56a
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-07-13 22:47:24 -0400

    Finished Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import { AiFillGithub } from "react-icons/ai";
+import logo from "../assets/images/logo.png";
 
 const Header = () => {
     return (
-        <div>
+        <div className="flex justify-between p-3 bg-gray-300">
             {/* left side */}
-            <div>
-                <div>
-                    <img src="../assets/logo.png" alt="Website logo" />
+            <div className="flex items-center gap-2">
+                <div className="w-10">
+                    <img src={logo} alt="Website logo" />
                 </div>
-                <h1>Meme Creator</h1>
+                <h1 className="font-bold text-sm">Meme Creator</h1>
             </div>
             {/* right side */}
-            <div>
-                <AiFillGithub />
-                <a href="#">GitHub Repo</a>
+            <div className="flex flex-col items-center">
+                <AiFillGithub className="github-icon" />
+                <a href="https://github.com/CDDR1/meme-generator-app" className="text-xs underline hover:no-underline" target="_blank">GitHub Repo</a>
             </div>
         </div>
     );
