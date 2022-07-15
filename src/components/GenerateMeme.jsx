b0080d6b16cf00ba514c4c705f2884770a0ad2f5
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-07-14 22:26:58 -0400

    Made project responsive

--- src/components/GenerateMeme.jsx
+++ src/components/GenerateMeme.jsx
@@ -4,14 +4,14 @@ const GenerateMeme = ({ memeTemplate, getNewMeme, bottomText, topText }) => {
             {
                 (bottomText.length > 0 && topText.length > 0) 
                 && 
-                <strong className="block font-medium">Click <a href="https://www.youtube.com/watch?v=a3Z7zEc7AXQ" target="_blank" className="underline hover:no-underline">here</a> to download your meme!</strong>
+                <strong className="block font-medium">Click <a href="https://www.youtube.com/watch?v=a3Z7zEc7AXQ" className="underline hover:no-underline">here</a> to download your meme!</strong>
             }
             <div className="relative">
-                <span className="meme-text block w-full absolute text-2xl uppercase font-semibold top-1 text-center">{topText}</span>
-                <img src={memeTemplate} alt="Meme template" className="block w-full" />
-                <span className="meme-text block w-full absolute text-2xl uppercase font-semibold bottom-1 text-center">{bottomText}</span>
+                <span className="meme-text block w-full absolute text-2xl sm:text-3xl lg:text-4xl uppercase font-semibold top-1 sm:top-3 lg:top-6 text-center">{topText}</span>
+                <img src={memeTemplate} alt="Meme template" className="block w-full lg:my-4" />
+                <span className="meme-text block w-full absolute text-2xl sm:text-3xl lg:text-4xl uppercase font-semibold bottom-1 sm:bottom-3 lg:bottom-6 text-center">{bottomText}</span>
             </div>
-            <button onClick={() => getNewMeme()} className="font-semibold text-white bg-gradient-to-r from-gradient-left to-gradient-right p-2 block w-full rounded-lg">Change meme template</button>
+            <button onClick={() => getNewMeme()} className="font-semibold text-white bg-gradient-to-r from-gradient-left to-gradient-right p-2 block w-full rounded-lg max-w-2xl lg:max-w-3xl">Change meme template</button>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import logo from "../assets/images/logo.png";
 
 const Header = () => {
     return (
-        <div className="flex justify-between text-white p-3 bg-gradient-to-r from-gradient-left to-gradient-right">
+        <div className="flex justify-between text-white p-3 lg:px-4 bg-gradient-to-r from-gradient-left to-gradient-right">
             {/* left side */}
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 sm:gap-4">
                 <div className="w-11">
                     <img src={logo} alt="Website logo" />
                 </div>
-                <h1 className="font-bold text-sm">Meme Creator</h1>
+                <h1 className="font-bold text-sm sm:text-2xl">Meme Creator</h1>
             </div>
             {/* right side */}
             <div className="flex flex-col items-center justify-center">
